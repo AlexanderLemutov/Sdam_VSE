8fc5ac4396fcfaafd344502aa86d950fff30e136
Alexander <j32lemutov@gmail.com>
2024-05-12 00:48:41 +0300

    icon added

--- src/main/java/com/example/project_gui/MainApplication.java
+++ src/main/java/com/example/project_gui/MainApplication.java
@@ -3,6 +3,7 @@ package com.example.project_gui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,8 +13,9 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), stage.getMaxWidth(), stage.getMaxHeight());
-        stage.setMaximized(true);
-        stage.setTitle("Abortysh");
+        Image image = new Image("file:///C:/Users/j32le/Documents/МГТУ им. Н.Э. Баумана/Разработка GUI приложений/Итоговый проект/Project_GUI/src/main/resources/Images/ege.png");
+        stage.getIcons().add(image);
+        stage.setTitle("ЕГЭ Физика 2024");
         stage.setScene(scene);
         stage.show();
     }
