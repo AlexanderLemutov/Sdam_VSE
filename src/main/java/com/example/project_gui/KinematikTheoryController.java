321706a55a0dccf05844b8dc18b16a16e9619ff2
Alexander <j32lemutov@gmail.com>
2024-05-18 22:14:30 +0300

    kinematik edited v1.1

--- src/main/java/com/example/project_gui/KinematikTheoryController.java
+++ src/main/java/com/example/project_gui/KinematikTheoryController.java
@@ -1,4 +1,21 @@
 package com.example.project_gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
 public class KinematikTheoryController {
+
+    @FXML
+    private Button returnBtn;
+
+    @FXML
+    void returnBtnClick(ActionEvent event) throws IOException {
+        sceneView.switchToScene(event, "mechTheory.fxml");
+    }
+
+    SceneController sceneView = new SceneController();
+
 }
