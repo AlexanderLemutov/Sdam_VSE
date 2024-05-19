c27c3239c73aa1ffd0d573707860aeafb603ef0a
Avaz <avaz.akmataliev@yandex.ru>
2024-05-19 20:08:26 +0300

    Av_quantum_pract

---
+++ src/main/java/com/example/project_gui/QuantumPractController.java
@@ -0,0 +1,53 @@
+package com.example.project_gui;
+
+import java.io.IOException;
+import java.util.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import static com.example.project_gui.Check.userData;
+
+public class QuantumPractController {
+
+    @FXML
+    private TextField answerField, answerField1, answerField2, answerField3, answerField4, answerField5;
+
+    @FXML
+    private Button chekButton;
+
+    @FXML
+    private Button returnButton;
+
+    @FXML
+    private ScrollPane scrollPane;
+
+    @FXML
+    void initialize() {
+        AnchorPane.setBottomAnchor(scrollPane, -1.5);
+        List<TextField> fieldList = new ArrayList<>(Arrays.asList(answerField, answerField1, answerField2, answerField3, answerField4, answerField5));
+        List<String> correctAnswerList = new ArrayList<>(Arrays.asList("29", "2", "50", "25", "92", "11"));
+
+        Check check = new Check(fieldList, correctAnswerList);
+        SceneController sceneView = new SceneController();
+        returnButton.setOnAction(actionEvent -> {
+            try {
+                sceneView.switchToScene(actionEvent,"pract.fxml");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        chekButton.setOnAction(actionEvent -> {
+            check.check("quantum");
+            scrollPane.setVvalue(0);
+        });
+        if (userData.getUserInputQuantum() != null) {
+            for (int i = 0; i < fieldList.size(); i++) {
+                fieldList.get(i).setText(userData.getUserInputQuantum().get(i));
+            }
+            check.check("quantum");
+        }
+    }
+}
--- src/main/java/com/example/project_gui/UserData.java
+++ src/main/java/com/example/project_gui/UserData.java
@@ -8,18 +8,22 @@ public class UserData {
     private ArrayList<String> answersStatik;
     private ArrayList<String> answersMolecular;
     private ArrayList<String> answersElectroDynamik;
+    private ArrayList<String> answersQuantum;
 
     public ArrayList<String> getUserInputKinematik() {return answersKinematik;}
     public ArrayList<String> getUserInputDynamik() {return answersDynamik;}
     public ArrayList<String> getUserInputStatik() {return answersStatik;}
     public ArrayList<String> getUserInputMolecular() {return answersMolecular;}
     public ArrayList<String> getUserInputElectroDynamik() {return answersElectroDynamik;}
+    public ArrayList<String> getUserInputQuantum() {return answersQuantum;}
+
 
     public void setUserInputKinematik(ArrayList<String> answersKinematik) {this.answersKinematik = answersKinematik;}
     public void setUserInputDynamik(ArrayList<String> answersDynamik) {this.answersDynamik = answersDynamik;}
     public void setUserInputStatik(ArrayList<String> answersStatik) {this.answersStatik = answersStatik;}
     public void setUserInputMolecular(ArrayList<String> answersMolecular) {this.answersMolecular = answersMolecular;}
     public void setUserInputElectroDynamik(ArrayList<String> answersElectroDynamik) {this.answersElectroDynamik = answersElectroDynamik;}
+    public void setUserInputQuantum(ArrayList<String> answersQuantum) {this.answersQuantum = answersQuantum;}
 
 
 }
